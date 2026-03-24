6318a578c4c924e9721fcf1282397124ae904e07
letsnotgothere0-droid <letsnotgothere0@gmail.com>
2026-03-25 01:00:05 +0530

    rewrite of the century

--- portfolio-backend/server.js
+++ portfolio-backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const Message = require('./models/message');
+const Message = require('./models/Message');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
